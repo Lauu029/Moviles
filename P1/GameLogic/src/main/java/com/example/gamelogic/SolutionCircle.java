e280a84fdcb160d20af130a4039556d6111c0549
Laura <lgomez25@ucm.es>
2023-11-03 19:26:04 +0100

    casi la lio muy fuerte

--- P1/GameLogic/src/main/java/com/example/gamelogic/SolutionCircle.java
+++ P1/GameLogic/src/main/java/com/example/gamelogic/SolutionCircle.java
@@ -4,29 +4,22 @@ import com.example.engine.IFont;
 import com.example.engine.IGraphics;
 import com.example.engine.TouchEvent;
 
+/* Clase correspondiente a los círculos de colores que se usan para adivinar la combinación.
+ * Extiende la clase Círculo y se encarga de avisar al GameManager cuando se ha pulsado uno para que
+ * almacene el color y lo coloque en la posición correspondiente*/
 public class SolutionCircle extends Circle {
 
     public SolutionCircle(String t, IFont f, int r, int x, int y, int row_) {
-        super(t,f, r, x, y, row_);
+        super(t, f, r, x, y, row_);
     }
 
     public boolean handleInput(TouchEvent event) {
-
-        if(event.type== TouchEvent.TouchEventType.TOUCH_UP)
-
-        if (this.posX - this.radius < event.x && this.posX + this.radius > event.x
-                && this.posY - this.radius < event.y && this.posY + this.radius > event.y) {
-            gm.takeColor(this.color, this.id_color);
-            return true;
-        }
+        if (event.type == TouchEvent.TouchEventType.TOUCH_UP)
+            if (this.posX - this.radius < event.x && this.posX + this.radius > event.x
+                    && this.posY - this.radius < event.y && this.posY + this.radius > event.y) {
+                gm.takeColor(this.color, this.id_color);
+                return true;
+            }
         return false;
-    }
-    public void render(IGraphics graph) {
-        super.render(graph);
-//        if (this.isDaltonics) {
-//            graph.setColor(0xFF000000);
-//            graph.setFont(this.font);
-//            graph.drawText(this.text, this.posX, this.posY);
-//        }
     }
 }
