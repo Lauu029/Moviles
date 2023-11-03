8353d3bb26a5d03c5000fb5a69d6a7cfc0b0e7f8
PaulaMorillasAlonso <pamorill@ucm.es>
2023-11-03 23:51:52 +0100

    no se que hacer

--- P1/DesktopEngine/src/main/java/com/example/desktopengine/AudioDesktop.java
+++ P1/DesktopEngine/src/main/java/com/example/desktopengine/AudioDesktop.java
@@ -17,184 +17,10 @@ import javax.sound.sampled.LineUnavailableException;
 
 public class AudioDesktop implements IAudio {
     // Implementacion de la interfaz IAudio
-    private ArrayList<Clip> myFreeSounds_;  // Lista de sonidos libres
-    private ArrayList<Clip> myUsedSounds_;  // Lista de sonidos en uso
+
     // Metodo para crear un nuevo objeto de sonido (SoundDesktop)
-    int maxSounds_;
-    public AudioDesktop(int maxSounds){
-        maxSounds_=maxSounds;
-        myFreeSounds_= new ArrayList<Clip>(); //inicializa la lista que contiene los sonidos disponibles
-        myUsedSounds_= new ArrayList<Clip>(); //inicializa la lista donde esten los sonidos en uso
 
-        for(int i=0;i<maxSounds_;i++){
-            myFreeSounds_.add(new Clip() {
-                @Override
-                public void open(AudioFormat audioFormat, byte[] bytes, int i, int i1) throws LineUnavailableException {
 
-                }
-
-                @Override
-                public void open(AudioInputStream audioInputStream) throws LineUnavailableException, IOException {
-
-                }
-
-                @Override
-                public int getFrameLength() {
-                    return 0;
-                }
-
-                @Override
-                public long getMicrosecondLength() {
-                    return 0;
-                }
-
-                @Override
-                public void setFramePosition(int i) {
-
-                }
-
-                @Override
-                public void setMicrosecondPosition(long l) {
-
-                }
-
-                @Override
-                public void setLoopPoints(int i, int i1) {
-
-                }
-
-                @Override
-                public void loop(int i) {
-
-                }
-
-                @Override
-                public void drain() {
-
-                }
-
-                @Override
-                public void flush() {
-
-                }
-
-                @Override
-                public void start() {
-
-                }
-
-                @Override
-                public void stop() {
-
-                }
-
-                @Override
-                public boolean isRunning() {
-                    return false;
-                }
-
-                @Override
-                public boolean isActive() {
-                    return false;
-                }
-
-                @Override
-                public AudioFormat getFormat() {
-                    return null;
-                }
-
-                @Override
-                public int getBufferSize() {
-                    return 0;
-                }
-
-                @Override
-                public int available() {
-                    return 0;
-                }
-
-                @Override
-                public int getFramePosition() {
-                    return 0;
-                }
-
-                @Override
-                public long getLongFramePosition() {
-                    return 0;
-                }
-
-                @Override
-                public long getMicrosecondPosition() {
-                    return 0;
-                }
-
-                @Override
-                public float getLevel() {
-                    return 0;
-                }
-
-                @Override
-                public Line.Info getLineInfo() {
-                    return null;
-                }
-
-                @Override
-                public void open() throws LineUnavailableException {
-
-                }
-
-                @Override
-                public void close() {
-
-                }
-
-                @Override
-                public boolean isOpen() {
-                    return false;
-                }
-
-                @Override
-                public Control[] getControls() {
-                    return new Control[0];
-                }
-
-                @Override
-                public boolean isControlSupported(Control.Type type) {
-                    return false;
-                }
-
-                @Override
-                public Control getControl(Control.Type type) {
-                    return null;
-                }
-
-                @Override
-                public void addLineListener(LineListener lineListener) {
-
-                }
-
-                @Override
-                public void removeLineListener(LineListener lineListener) {
-
-                }
-            });
-        }
-    }
-    public Clip getFreeClip(){
-        if(!myFreeSounds_.isEmpty()) { //Si hay hueco para crear un nuevo sonido
-            return myFreeSounds_.remove(myFreeSounds_.size() - 1); //Quitamos el hueco disponible
-        } else {
-            throw new IllegalStateException("No hay objetos disponibles en el pool.");
-        }
-    }
-    private void returnClip(Clip c) {
-        if (myUsedSounds_.contains(c)) {
-            myUsedSounds_.remove(c);
-            myUsedSounds_.add(c);
-        } else {
-            throw new IllegalArgumentException("El objeto no está en uso.");
-        }
-    }
     @Override
     public SoundDesktop newSound(String file, String id) {
         SoundDesktop sound= new SoundDesktop(file,id);
@@ -206,7 +32,7 @@ public class AudioDesktop implements IAudio {
     @Override
     public void playSound(ISound sound, int loop) {
         SoundDesktop dSound = (SoundDesktop) sound; // Convierte el objeto ISound a SoundDesktop
-        Clip clip = dSound.getClip(); // Obtiene el clip de sonido asociado
+        Clip clip = dSound.getFreeClip(); // Obtiene el clip de sonido asociado
 
         if (clip != null) { // Verifica si el clip no es nulo
             if (loop != 0) {
@@ -218,9 +44,9 @@ public class AudioDesktop implements IAudio {
     }
 
     // Metodo para detener la reproducción de un sonido
-    @Override
+   @Override
     public void stopSound(ISound sound) {
-        SoundDesktop dSound = (SoundDesktop) sound; // Convierte el objeto ISound a SoundDesktop
+         /*SoundDesktop dSound = (SoundDesktop) sound; // Convierte el objeto ISound a SoundDesktop
         Clip clip = dSound.getClip(); // Obtiene el clip de sonido asociado
 
         if (clip != null && myUsedSounds_.contains(clip)) { // Verifica si el clip no es nulo y esta en uso
@@ -230,6 +56,6 @@ public class AudioDesktop implements IAudio {
             clip.close(); // Cierra el clip para liberar recursos
         }else {
             throw new IllegalArgumentException("El sonido no se puede pausar, no está en uso.");
-        }
+        }*/
     }
 }
--- P1/DesktopEngine/src/main/java/com/example/desktopengine/SoundDesktop.java
+++ P1/DesktopEngine/src/main/java/com/example/desktopengine/SoundDesktop.java
@@ -3,39 +3,72 @@ package com.example.desktopengine;
 import com.example.engine.ISound;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.Control;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.LineListener;
+import javax.sound.sampled.LineUnavailableException;
 
 public class SoundDesktop implements ISound {
     private String path_ = "Assets/"; // Ruta predeterminada para buscar archivos de sonido
     private Clip myClip_; // Objeto Clip para representar el sonido
 
     // Constructor de la clase
+    private ArrayList<Clip> myFreeSounds_;  // Lista de sonidos libres
+    private ArrayList<Clip> myUsedSounds_;  // Lista de sonidos en uso
+    int maxSounds_;
     public SoundDesktop(String file, String id) {
-        myClip_ = null;
+
+        maxSounds_=5;
+        myFreeSounds_= new ArrayList<Clip>(); //inicializa la lista que contiene los sonidos disponibles
+        myUsedSounds_= new ArrayList<Clip>(); //inicializa la lista donde esten los sonidos en uso
+
+
         try {
             File audioFile = new File(path_ + file); // Crea un objeto File para el archivo de sonido
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile); // Obtiene un flujo de audio a partir del archivo
-            myClip_ = AudioSystem.getClip(); // Crea un nuevo Clip
-            myClip_.open(audioStream); // Abre el Clip y carga el audio desde el flujo
-
+            for(int i=0;i<maxSounds_;i++){
+                Clip c=AudioSystem.getClip();
+                c.open(audioStream); // Abre el Clip y carga el audio desde el flujo
+                myFreeSounds_.add(c);
+            }
         } catch (Exception e) {
             System.err.println("Couldn't load audio file"); // Imprime un mensaje de error si no se puede cargar el archivo de audio
             e.printStackTrace(); // Imprime la traza de la excepcion
         }
     }
-
     // Método para obtener el Clip de sonido
-    public Clip getClip() {
-        Clip currClip = myClip_;
-        if (currClip.isOpen()) {
-            return currClip; // Devuelve el Clip si está abierto y listo para su reproducción
+    /*public Clip getUsedClip() {
+
+        if (myClip_.isOpen()) {
+            return myClip_; // Devuelve el Clip si está abierto y listo para su reproducción
+        }
+        else {
+            throw new IllegalStateException("No hay ningún clip de est sonido en uso.");
+        }
+    }*/
+    public Clip getFreeClip(){
+        if(!myFreeSounds_.isEmpty()) { //Si hay hueco para crear un nuevo sonido
+            Clip c=myFreeSounds_.remove(myFreeSounds_.size() - 1);
+            return c; //Quitamos el hueco disponible
         } else {
-            System.out.print("The sound you're trying to play couldn't be found\n");
-            return null; // Devuelve nulo si el Clip no se pudo encontrar
+            return null;
         }
     }
+    private void returnClip(Clip c) {
+        if (myUsedSounds_.contains(c)) {
+            myUsedSounds_.remove(c);
+            myUsedSounds_.add(c);
+        } else {
+            throw new IllegalArgumentException("El objeto no está en uso.");
+        }
+    }
+
 }
