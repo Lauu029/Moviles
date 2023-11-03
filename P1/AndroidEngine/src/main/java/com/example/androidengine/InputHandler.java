e01deed11069dbf616c399cf2715afeccaac6e13
MiriamMartín <mirima10@ucm.es>
2023-11-03 13:01:06 +0100

    pool de ventos en input

--- P1/AndroidEngine/src/main/java/com/example/androidengine/InputHandler.java
+++ P1/AndroidEngine/src/main/java/com/example/androidengine/InputHandler.java
@@ -11,10 +11,18 @@ import java.util.ArrayList;
 public class InputHandler implements View.OnTouchListener{
     private ArrayList<TouchEvent> myTouchEvent_;
     private ArrayList<TouchEvent> myPendingEvents_;
+    private ArrayList<TouchEvent> usedEvnets;
+    private ArrayList<TouchEvent> freeEvnets;
+    private int maxEvents=10;
     InputHandler(SurfaceView view){
         myTouchEvent_=new ArrayList<TouchEvent>();
         myPendingEvents_=new ArrayList<TouchEvent>();
+        usedEvnets=new ArrayList<TouchEvent>();
+        freeEvnets=new ArrayList<TouchEvent>();
         view.setOnTouchListener( this);
+        for(int i=0;i<maxEvents;i++){
+            freeEvnets.add(new TouchEvent());
+        }
     }
     public ArrayList<TouchEvent>getMyPendingEvents_(){
         return myPendingEvents_;
@@ -22,9 +30,26 @@ public class InputHandler implements View.OnTouchListener{
     public void myPendingEventsClear(){
         myPendingEvents_.clear();
     }
+    private TouchEvent getEvent() {
+        if (!freeEvnets.isEmpty()) {
+            TouchEvent obj = freeEvnets.remove(freeEvnets.size() - 1);
+            usedEvnets.add(obj);
+            return obj;
+        } else {
+            throw new IllegalStateException("No available objects in the pool.");
+        }
+    }
+    private void returnObject(TouchEvent obj) {
+        if (usedEvnets.contains(obj)) {
+            usedEvnets.remove(obj);
+            freeEvnets.add(obj);
+        } else {
+            throw new IllegalArgumentException("The object is not in use.");
+        }
+    }
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        TouchEvent event=new TouchEvent();
+        TouchEvent event=getEvent();
 
         int index=motionEvent.getActionIndex();
         int finger=motionEvent.getPointerId(index);
@@ -42,6 +67,7 @@ public class InputHandler implements View.OnTouchListener{
         synchronized (this){
             myPendingEvents_.add(event);
         }
+        returnObject(event);
         return true;
     }
 }
--- P1/DesktopEngine/src/main/java/com/example/desktopengine/InputHandler.java
+++ P1/DesktopEngine/src/main/java/com/example/desktopengine/InputHandler.java
@@ -11,10 +11,36 @@ import javax.swing.JFrame;
 public class InputHandler implements MouseListener {
     private ArrayList<TouchEvent> myTouchEvent_;
     private ArrayList<TouchEvent> myPendingEvents_;
+    private ArrayList<TouchEvent> usedEvnets;
+    private ArrayList<TouchEvent> freeEvnets;
+    private int maxEvents=10;
     InputHandler(JFrame jframe){
         jframe.addMouseListener(this);
         myTouchEvent_=new ArrayList<TouchEvent>();
         myPendingEvents_=new ArrayList<TouchEvent>();
+        usedEvnets=new ArrayList<TouchEvent>();
+        freeEvnets=new ArrayList<TouchEvent>();
+
+        for(int i=0;i<maxEvents;i++){
+            freeEvnets.add(new TouchEvent());
+        }
+    }
+    private TouchEvent getEvent() {
+        if (!freeEvnets.isEmpty()) {
+            TouchEvent obj = freeEvnets.remove(freeEvnets.size() - 1);
+            usedEvnets.add(obj);
+            return obj;
+        } else {
+            throw new IllegalStateException("No available objects in the pool.");
+        }
+    }
+    private void returnObject(TouchEvent obj) {
+        if (usedEvnets.contains(obj)) {
+            usedEvnets.remove(obj);
+            freeEvnets.add(obj);
+        } else {
+            throw new IllegalArgumentException("The object is not in use.");
+        }
     }
     @Override
     public void mouseClicked(MouseEvent mouseEvent) {
@@ -29,24 +55,26 @@ public class InputHandler implements MouseListener {
 
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
-        TouchEvent event=new TouchEvent();
+        TouchEvent event=getEvent();
         event.x=mouseEvent.getX();
         event.y=mouseEvent.getY();
         event.type= TouchEvent.TouchEventType.TOUCH_DOWN;
         synchronized (this){
             myPendingEvents_.add(event);
         }
+        returnObject(event);
     }
 
     @Override
     public void mouseReleased(MouseEvent mouseEvent) {
-        TouchEvent event=new TouchEvent();
+        TouchEvent event=getEvent();
         event.x=mouseEvent.getX();
         event.y=mouseEvent.getY();
         event.type= TouchEvent.TouchEventType.TOUCH_UP;
         synchronized (this){
             myPendingEvents_.add(event);
         }
+        returnObject(event);
     }
 
     @Override
