11b52834fe258609918164927d1dbc587ad5645b
Laura <lgomez25@ucm.es>
2023-11-21 22:54:44 +0100

    Intento de poner un banner

--- P2_Grupo02/P2/Game/src/main/java/com/example/androidgame/MainActivity.java
+++ P2_Grupo02/P2/Game/src/main/java/com/example/androidgame/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.androidgame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 //import android.widget.RelativeLayout;
 
 import com.example.androidengine.Engine;
 import com.example.androidgame.GameLogic.GameManager;
 import com.example.androidgame.GameLogic.MenuScene;
-
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 public class MainActivity extends AppCompatActivity {
     private SurfaceView renderView_;
     private Engine IEngine_;
@@ -30,6 +39,23 @@ public class MainActivity extends AppCompatActivity {
 
         IEngine_.resume();
         gm.init();
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+
+            }
+        });
+        
+        AdView adView_=(AdView)findViewById(R.id.adView);
+        if (adView_ != null) {
+            Log.d("MainActivity", "AdView found, loading ad...");
+            AdRequest adRequest = new AdRequest.Builder().build();
+            adView_.loadAd(adRequest);
+        } else {
+            Log.e("MainActivity", "AdView is null");
+        }
+        //AdRequest adRequest = new AdRequest.Builder().build();
+        //adView_.loadAd(adRequest);
     }
 
     @Override
--- P2_Grupo02/P2/settings.gradle.kts
+++ P2_Grupo02/P2/settings.gradle.kts
@@ -14,6 +14,5 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "MasterMind"
-include(":GameLogic")
 include(":AndroidEngine")
 include(":Game")
