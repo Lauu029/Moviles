29fe70451e2600a4f72842c78ade759cca63832d
Laura <lgomez25@ucm.es>
2023-11-23 19:21:51 +0100

    Anuncios rewarded

--- P2_Grupo02/P2/AndroidEngine/src/main/java/com/example/androidengine/Mobile.java
+++ P2_Grupo02/P2/AndroidEngine/src/main/java/com/example/androidengine/Mobile.java
@@ -13,16 +13,21 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.OnUserEarnedRewardListener;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 
 public class Mobile {
     private Context context_;
     private AdView adView_;
     private AdRequest adRequest_;
     private Activity myActivity_;
+    private RewardedAd rewardedAd_;
     public Mobile(Context c, Activity activity) {
         this.context_ = c;
         this.myActivity_=activity;
@@ -32,8 +37,19 @@ public class Mobile {
                 Log.d("MainActivity", "Starting");
             }
         });
-
         adRequest_ = new AdRequest.Builder().build();
+        RewardedAd.load(context_, "ca-app-pub-3940256099942544/5224354917", adRequest_, new RewardedAdLoadCallback() {
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                Log.d("MainActivity", loadAdError.toString());
+                rewardedAd_ = null;
+            }
+            @Override
+            public void onAdLoaded(@NonNull RewardedAd ad) {
+                rewardedAd_ = ad;
+                Log.d("MainActivity", "Rewarded ad was loaded.");
+            }
+        });
     }
 
     public void loadAdBanner(AdView adView) {
@@ -46,35 +62,35 @@ public class Mobile {
             Log.e("MainActivity", "AdView is null");
         }
     }
-    public void shareImage(Bitmap bitmap, String msj){
-        String pathBitmap= MediaStore.Images.Media.insertImage(context_.getContentResolver(),bitmap,"titulo","descripcion");
-        Uri uri= Uri.parse(pathBitmap);
-        Intent shareIntent = new Intent(android.content.Intent. ACTION_SEND);
+    public void shareImage(Bitmap bitmap, String msj) {
+        String pathBitmap = MediaStore.Images.Media.insertImage(context_.getContentResolver(), bitmap, "titulo", "descripcion");
+        Uri uri = Uri.parse(pathBitmap);
+        Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
         shareIntent.setType("image/*");
-        shareIntent.putExtra(Intent.EXTRA_TEXT,msj);
-        shareIntent.putExtra(Intent.EXTRA_STREAM,uri);
-        this.myActivity_.startActivity(Intent.createChooser(shareIntent,"ActicityTitle"));
-
+        shareIntent.putExtra(Intent.EXTRA_TEXT, msj);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        this.myActivity_.startActivity(Intent.createChooser(shareIntent, "ActicityTitle"));
+    }
     public void Init() {
 
     }
 
     public void LoadRewardedAd(){
-        mainActivity.runOnUiThread(new Runnable() {
+        myActivity_.runOnUiThread(new Runnable() {
             @Override public void run() {
-                if (mRewardedAd != null) {
-                    mRewardedAd.show(mainActivity, new OnUserEarnedRewardListener() {
+                if (rewardedAd_ != null) {
+                    rewardedAd_.show(myActivity_, new OnUserEarnedRewardListener() {
                         @Override
                         public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
                             // Handle the reward.
-                            Log.d(TAG, "The user earned the reward.");
+                            Log.d("MainActivity", "The user earned the reward.");
                             //int rewardAmount = rewardItem.getAmount();
                             //String rewardType = rewardItem.getType();
                         }
                     });
                 } else {
                     System.out.println("The rewarded ad wasn't ready yet.");
-                    Log.d(TAG, "The rewarded ad wasn't ready yet.");
+                    Log.d("MainActivity", "The rewarded ad wasn't ready yet.");
                 }
             }
         });
