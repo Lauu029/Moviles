f0725f59b9eeb7b7c6dfb23365ea8a7c1626baac
Laura <lgomez25@ucm.es>
2023-11-23 18:43:04 +0100

    Update Mobile.java

--- P2_Grupo02/P2/AndroidEngine/src/main/java/com/example/androidengine/Mobile.java
+++ P2_Grupo02/P2/AndroidEngine/src/main/java/com/example/androidengine/Mobile.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
@@ -19,7 +20,7 @@ public class Mobile {
 
     public Mobile(Context c) {
         this.context_ = c;
-       MobileAds.initialize(this.context_, new OnInitializationCompleteListener() {
+        MobileAds.initialize(this.context_, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
                 Log.d("MainActivity", "Starting");
@@ -40,21 +41,28 @@ public class Mobile {
         }
     }
 
-    public void Init(Engine e) {
-        //Activity main = iEngine_.getMainActivity();
-        /*main.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                AdView adView_= (AdView) main.findViewById(com.example.androidgame.R.id.adView);
-                if (adView_ != null) {
-                    Log.d("MainActivity", "AdView found, loading ad...");
-                    AdRequest adRequest = new AdRequest.Builder().build();
-                    adView_.loadAd(adRequest);
-                    adView_.setVisibility(View.VISIBLE);
+    public void Init() {
+
+    }
+
+    public void LoadRewardedAd(){
+        mainActivity.runOnUiThread(new Runnable() {
+            @Override public void run() {
+                if (mRewardedAd != null) {
+                    mRewardedAd.show(mainActivity, new OnUserEarnedRewardListener() {
+                        @Override
+                        public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                            // Handle the reward.
+                            Log.d(TAG, "The user earned the reward.");
+                            //int rewardAmount = rewardItem.getAmount();
+                            //String rewardType = rewardItem.getType();
+                        }
+                    });
                 } else {
-                    Log.e("MainActivity", "AdView is null");
+                    System.out.println("The rewarded ad wasn't ready yet.");
+                    Log.d(TAG, "The rewarded ad wasn't ready yet.");
                 }
             }
-        });*/
+        });
     }
 }
