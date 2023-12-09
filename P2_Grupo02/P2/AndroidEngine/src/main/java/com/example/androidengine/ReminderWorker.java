745a265b9a23783d1e5e1307c97967ba5cae1ed8
MiriamMartín <mirima10@ucm.es>
2023-12-09 02:34:55 +0100

    no se no sale

--- P2_Grupo02/P2/AndroidEngine/src/main/java/com/example/androidengine/ReminderWorker.java
+++ P2_Grupo02/P2/AndroidEngine/src/main/java/com/example/androidengine/ReminderWorker.java
@@ -9,16 +9,17 @@ import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 public class ReminderWorker extends Worker {
-    Mobile mobile;
-    public ReminderWorker(@NonNull Context context, @NonNull WorkerParameters params) {
+     Mobile mobile;
+    public ReminderWorker(@NonNull Context context, @NonNull WorkerParameters params,Mobile mobile) {
         super(context, params);
     }
 
     @NonNull
     @Override
     public Result doWork() {
-
+        mobile.createNotification(0);
         Log.d("GAME","HOLAAAAAAA K LOKO");
         return Result.success();
     }
+
 }
