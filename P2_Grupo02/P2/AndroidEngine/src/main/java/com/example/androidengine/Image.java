01ac0b0250a11ed5c3ffca57717f00819a971cee
PaulaMorillasAlonso <pamorill@ucm.es>
2023-12-22 13:52:29 +0100

    avance visibilidad imagenes

--- P2_Grupo02/P2/AndroidEngine/src/main/java/com/example/androidengine/Image.java
+++ P2_Grupo02/P2/AndroidEngine/src/main/java/com/example/androidengine/Image.java
@@ -5,13 +5,15 @@ import android.graphics.Bitmap;
 
 public class Image {
     private Bitmap bitmap_; //Mapa de bits necesario para generar la imagen en android
+    private boolean isVisible_;
     //Constructor de la clase
     public Image(Bitmap image_){
         bitmap_ =image_;
+        isVisible_ = true;
     }
     //Devuelve el mapa de bits usado para crear la imagen
     public Bitmap getImage(){
-        return bitmap_;
+        return isVisible_ ? bitmap_ : null;
     }
     //Devuelve el ancho del mapa de bits
     public int getWidth() {
@@ -21,4 +23,11 @@ public class Image {
     public int getHeight() {
         return bitmap_.getHeight();
     }
+    public void setVisibility(boolean isVisible) {
+        isVisible_ = isVisible;
+    }
+    public boolean isVisible() {
+        return isVisible_;
+    }
+
 }
