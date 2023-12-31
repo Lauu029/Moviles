ba81683ff5bbb60a9a700a9fdebf2943b5390e40
MiriamMartín <mirima10@ucm.es>
2023-12-31 17:25:45 +0100

    cambio input

--- P1_Grupo02/P1/AndroidEngine/src/main/java/com/example/androidengine/InputHandler.java
+++ P1_Grupo02/P1/AndroidEngine/src/main/java/com/example/androidengine/InputHandler.java
@@ -1,5 +1,6 @@
 package com.example.androidengine;
 
+
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
@@ -9,14 +10,12 @@ import com.example.engine.TouchEvent;
 import java.util.ArrayList;
 
 public class InputHandler implements View.OnTouchListener {
-    private ArrayList<TouchEvent> myTouchEvent_; // Lista de eventos de entrada
     private ArrayList<TouchEvent> myPendingEvents_; // Lista de eventos pendientes
     private ArrayList<TouchEvent> usedEvents_; // Lista de eventos en uso
     private ArrayList<TouchEvent> freeEvents_;  // Lista de eventos libres
-    private int maxEvents_ = 10; // Numero máximo de eventos en el pool
+    private int maxEvents_ = 1000; // Numero máximo de eventos en el pool
 
     InputHandler(SurfaceView view) {
-        myTouchEvent_ = new ArrayList<TouchEvent>(); // Inicializa la lista de eventos táctiles
         myPendingEvents_ = new ArrayList<TouchEvent>(); // Inicializa la lista de eventos pendientes
         usedEvents_ = new ArrayList<TouchEvent>(); // Inicializa la lista de eventos en uso
         freeEvents_ = new ArrayList<TouchEvent>(); // Inicializa la lista de eventos libres
@@ -30,7 +29,7 @@ public class InputHandler implements View.OnTouchListener {
         return myPendingEvents_;
     }
     // Limpia la lista de eventos pendientes
-    public void myPendingEventsClear() {
+    public synchronized void  myPendingEventsClear() {
         myPendingEvents_.clear();
     }
     // Obtiene un objeto TouchEvent del pool
@@ -44,47 +43,55 @@ public class InputHandler implements View.OnTouchListener {
         }
     }
     // Devuelve un objeto TouchEvent al pool
-    private void returnObject(TouchEvent obj) {
-        if (usedEvents_.contains(obj)) {
-            usedEvents_.remove(obj);
-            freeEvents_.add(obj);
-        } else {
-            throw new IllegalArgumentException("The object is not in use.");
+    void  FreePool(){
+
+        while (!usedEvents_.isEmpty()) {
+            TouchEvent event = usedEvents_.remove(0);
+            freeEvents_.add(event);
         }
     }
     //Se llama cuando un evento touch se envía a una vista
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
 
-
         int index = motionEvent.getActionIndex(); //Devuelve el índice de puntero asociado
-        int finger = motionEvent.getPointerId(index); //Dedo que realiza el toque (pantallas multitactil)
-        int action = motionEvent.getActionMasked();
 
+        int action = motionEvent.getActionMasked();
 
         if (action == motionEvent.ACTION_DOWN) { //Comprueba que tipo de accion ha realizado (pulsar, levantar)
             TouchEvent event = getEvent();
-            event.x = (int) motionEvent.getX(finger); //Obtiene las coordenadas donde ha tenido lugar la accion
-            event.y = (int) motionEvent.getY(finger);
+            event.x = (int) motionEvent.getX(index); //Obtiene las coordenadas donde ha tenido lugar la accion
+            event.y = (int) motionEvent.getY(index);
+
             event.type = TouchEvent.TouchEventType.TOUCH_DOWN;
             synchronized (this) { //Es necesario sincronizarlo para evitar fallos al borrar los elementos de esta lista
                 myPendingEvents_.add(event);
             }
-            returnObject(event);
+            //returnObject(event);
         } else if (action == motionEvent.ACTION_UP) {
             TouchEvent event = getEvent();
-            event.x = (int) motionEvent.getX(finger); //Obtiene las coordenadas donde ha tenido lugar la accion
-            event.y = (int) motionEvent.getY(finger);
+            event.x = (int) motionEvent.getX(index); //Obtiene las coordenadas donde ha tenido lugar la accion
+            event.y = (int) motionEvent.getY(index);
             event.type = TouchEvent.TouchEventType.TOUCH_UP;
             synchronized (this) { //Es necesario sincronizarlo para evitar fallos al borrar los elementos de esta lista
                 myPendingEvents_.add(event);
             }
-            returnObject(event);
+            //returnObject(event);
+        }
+        else if (action==motionEvent.ACTION_MOVE) {
+            TouchEvent event = getEvent();
+            event.type = TouchEvent.TouchEventType.TOUCH_DRAG;
+
+            event.x = (int)motionEvent.getX(index);
+            event.y=(int)motionEvent.getY(index);
+
+            synchronized (this) { //Es necesario sincronizarlo para evitar fallos al borrar los elementos de esta lista
+                myPendingEvents_.add(event);
+            }
 
         }
-
-
-
         return true;
     }
+
+
 }
